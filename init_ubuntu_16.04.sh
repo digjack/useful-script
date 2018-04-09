a1bba02eea1be35862633825e00cb2f1cae2f5ed
banli <banli@huanleguang.com>
2018-04-09 10:11:52 +0800

    optimize init file

--- init_ubuntu_16.04.sh
+++ init_ubuntu_16.04.sh
@@ -5,6 +5,16 @@ apt-get install zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+
+# add ssh key
+file="~/.ssh/authorized_keys"
+key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXpQ4X3ixKAMwlEmdhh7ODntdRonY5zjMBRIXKlY5R4n28rSwgzsTjIqhVRwnPnMnfLOe7jUrybkhZYWPmic4DNT7fvzDXPRfBnTPSj5mW8As9kEZqGGtjovyW4i7BUpVr3IgBNcvGj/LINwJ4fwbo1qavb31T+U4Q+aIYh7SNXaSdnr2kuYcVoSCn/PqnnFAHUwhXHyHc9M1cs7mxmlBZDahiIwO6btZTtxxAOASH9tlDU1ZBBDi9l6ChvWb2yZzdJk20ZV28ulAunW+E3FAaDGGDxc57mjWPzQfKvGiUwztGTtt2aiUHl2kJnd0mXYPC4TWsClRJTIZRgYa71Z8t banli@BanlideMacBook-Air.local"
+if [ -f "$file" ]
+then
+	touch $file
+
+echo $key >> $file
+
 #软件源设置  参考:https://mirrors.ustc.edu.cn/repogen/
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 
