7c07d3cb41563fae9a3554b8ab89f38a379620ef
banli <244541048@qq.com>
2018-04-10 01:09:07 +0800

    init

--- ubuntu_16.04_init.sh
+++ ubuntu_16.04_init.sh
@@ -1,19 +1,6 @@
 #! /bin/bash
 
-# zsh
-apt-get install zsh
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-
-# add ssh key
-file="~/.ssh/authorized_keys"
-key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXpQ4X3ixKAMwlEmdhh7ODntdRonY5zjMBRIXKlY5R4n28rSwgzsTjIqhVRwnPnMnfLOe7jUrybkhZYWPmic4DNT7fvzDXPRfBnTPSj5mW8As9kEZqGGtjovyW4i7BUpVr3IgBNcvGj/LINwJ4fwbo1qavb31T+U4Q+aIYh7SNXaSdnr2kuYcVoSCn/PqnnFAHUwhXHyHc9M1cs7mxmlBZDahiIwO6btZTtxxAOASH9tlDU1ZBBDi9l6ChvWb2yZzdJk20ZV28ulAunW+E3FAaDGGDxc57mjWPzQfKvGiUwztGTtt2aiUHl2kJnd0mXYPC4TWsClRJTIZRgYa71Z8t banli@BanlideMacBook-Air.local"
-if [ -f "$file" ]
-then
-	touch $file
-
-echo $key >> $file
 
 #软件源设置  参考:https://mirrors.ustc.edu.cn/repogen/
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
@@ -35,4 +22,26 @@ deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-backports main restricted univ
 # deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
 # deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse" >/etc/apt/sources.list
 
+
+# zsh
+apt-get install zsh
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
+# add ssh key
+if [ ! -d ~/.ssh ]
+then
+    mkdir ~/.ssh
+fi
+
+file="~/.ssh/authorized_keys"
+key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXpQ4X3ixKAMwlEmdhh7ODntdRonY5zjMBRIXKlY5R4n28rSwgzsTjIqhVRwnPnMnfLOe7jUrybkhZYWPmic4DNT7fvzDXPRfBnTPSj5mW8As9kEZqGGtjovyW4i7BUpVr3IgBNcvGj/LINwJ4fwbo1qavb31T+U4Q+aIYh7SNXaSdnr2kuYcVoSCn/PqnnFAHUwhXHyHc9M1cs7mxmlBZDahiIwO6btZTtxxAOASH9tlDU1ZBBDi9l6ChvWb2yZzdJk20ZV28ulAunW+E3FAaDGGDxc57mjWPzQfKvGiUwztGTtt2aiUHl2kJnd0mXYPC4TWsClRJTIZRgYa71Z8t banli@BanlideMacBook-Air.local"
+if [ ! -f ~/.ssh/authorized_keys ]
+then
+	touch ~/.ssh/authorized_keys
+fi
+echo $key >> ~/.ssh/authorized_keys
+
  
+
