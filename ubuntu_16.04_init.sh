4baeae414bfe9a9ead3e7a3c8b4833bff4c9fce4
digjack <244541048@qq.com>
2018-05-08 11:51:27 +0800

    Update ubuntu_16.04_init.sh
    add awesome vim

--- ubuntu_16.04_init.sh
+++ ubuntu_16.04_init.sh
@@ -27,8 +27,12 @@ apt-get update
 #base software
 apt-get install git vim curl zsh
 
+#zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+#awesome vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # add ssh key
 if [ ! -d ~/.ssh ]
